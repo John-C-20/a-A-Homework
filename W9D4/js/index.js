4f36691385ae50e8d892d5bfb0d673c5fc206904
John <johncheung1997@gmail.com>
2021-02-25 07:51:09 -0500

    W9D4

--- W9D4/js/index.js
+++ W9D4/js/index.js
@@ -1,15 +1,24 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
+
 $.ajax({
     type: 'get',
     url: 'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b',
     success: function(data) {
         // Object.keys(data).forEach (ele => console.log(ele, data[ele]))
-        console.log(data['weather'][0])
+        const dataObj = data
+        console.log(dataObj['weather'][0])
         console.log('ajax is asynchronous')
+        makeHTML(dataObj)
     }  
 })
 
 // Add another console log here, outside your AJAX request
 console.log('this will print before ajax')
+
+
+function makeHTML(data) { 
+    $('body').append('<p> Today\'s weather is ' + data['weather'][0]['main'] + '</p>')
+    $('body').append('<p> We will be having ' + data['weather'][0]['description'] + ' in ' + data['name'] + '</p>')
+}
